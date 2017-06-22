ce715167decd16121557a6791c1d7ced18fe64b8
Ravinder <ravinder@number7even.de>
2017-06-22 22:57:15 +0530

    added feture for blank module

--- src/Generators/Scaffold/ControllerGenerator.php
+++ src/Generators/Scaffold/ControllerGenerator.php
@@ -29,75 +29,81 @@ class ControllerGenerator extends BaseGenerator
     }
 
     public function generate()
-    {
-        if ($this->commandData->getAddOn('datatables')) {
-            $templateData = get_template('scaffold.controller.datatable_controller', 'laravel-generator');
-
-            $this->generateDataTable();
-        } else {
-            $templateData = get_template('scaffold.controller.controller', 'laravel-generator');
-
-            $paginate = $this->commandData->getOption('paginate');
-
-            if ($paginate) {
-                $templateData = str_replace('$RENDER_TYPE$', 'paginate('.$paginate.')', $templateData);
-            } else {
-                $templateData = str_replace('$RENDER_TYPE$', 'all()', $templateData);
-            }
-        }
-
-        $module_config = \DB::table('modbuilder_mob')->where('slug_mob','=',$this->commandData->modelName)->first();
-        $mod = json_decode($module_config->module_config);
+    {   
+         $module_config = \DB::table('modbuilder_mob')->where('slug_mob','=','testdba')->first();
+         // $module_config = \DB::table('modbuilder_mob')->where('slug_mob','=',$this->commandData->modelName'')->first();
+         if($module_config->feature_type_mob==1){
         
+            if ($this->commandData->getAddOn('datatables')) {
+                $templateData = get_template('scaffold.controller.datatable_controller', 'laravel-generator');
 
-        $templateData = fill_template($this->commandData->dynamicVars, $templateData);
-        //print_r($templateData);
-		$validationMsgStr ="\n\t ".'$messages = [';
-        $validationStr ="\n\t".'$validator = Validator::make($request->all(), ['."\n";
-        $validateRule = false;
-        if(isset($mod->module_info->fields) && $mod->module_info->fields!=''){
-            $validationStrCon ='';
-             $validationStrConEdit ='';
-            $fieldsArr = json_decode($mod->module_info->fields);
-            foreach($fieldsArr as $fieldKey => $fieldValue) {
-                 //$actualJsonFormat[$fieldKey]['validation'] = $fieldValue->name;
+                $this->generateDataTable();
+            } else {
+                $templateData = get_template('scaffold.controller.controller', 'laravel-generator');
 
-                
-                if(isset($fieldValue->validation)){
-                    $validationValueStr = '';
-					$validationMsgsStr = '';
-                    foreach($fieldValue->validation as $validationKey => $validationValue) {
-                       $validationValueStr.=$validationValue->rule.'|'; 
-					   $validationMsgsStr.="'".$fieldValue->name.".".$validationValue->rule."' => trans('".$module_config->slug_mob.".admin_".$module_config->slug_mob."_module_error_".$fieldValue->name."_".$validationValue->rule."'),"."\n";
-                    }
-                
-                    $validationStr .= "\t\t"."'".$fieldValue->name."' => '".substr($validationValueStr,0,-1)."',";  
-					$validationMsgStr .= $validationMsgsStr;
-                    $validateRule = true;
-                } 
-            } 
-			$validationMsgStr .='];'."\n";
-            $validationStr = substr($validationStr,0,-1)."\n";
-            $validationStr .="\t".'],$messages);'."\n";
-            $validationStrCon .= 'if ($validator->fails()) {'."\n";
-            $validationStrCon .= 'return redirect(\'admin/'.str_plural($module_config->slug_mob).'/create\')->withErrors($validator)->withInput();'."\n";
+                $paginate = $this->commandData->getOption('paginate');
+
+                if ($paginate) {
+                    $templateData = str_replace('$RENDER_TYPE$', 'paginate('.$paginate.')', $templateData);
+                } else {
+                    $templateData = str_replace('$RENDER_TYPE$', 'all()', $templateData);
+                }
+            }
+
+           
+            $mod = json_decode($module_config->module_config);
             
-            $validationStrCon .='}'."\n";
-            $validationStrConEdit .= 'if ($validator->fails()) {'."\n";
-            $validationStrConEdit .= 'return redirect(\'admin/'.str_plural($module_config->slug_mob).'/\'.$id.\'/edit\')->withErrors($validator)->withInput();'."\n";
-            $validationStrConEdit .='}'."\n";  
-        }
 
-		if($validateRule==true){
-            $validatorValues = $validationMsgStr."\n".$validationStr.$validationStrCon;
-            $validatorValuesEdit = $validationMsgStr."\n".$validationStr.$validationStrConEdit;
-        }else{
-            $validatorValues = '';
-            $validatorValuesEdit = '';
-        }    
-        $templateData = str_replace('$VAILDATIONS$', $validatorValues, $templateData);
-        $templateData = str_replace('$EDIT_VAILDATIONS$', $validatorValuesEdit, $templateData);
-       
+            $templateData = fill_template($this->commandData->dynamicVars, $templateData);
+            //print_r($templateData);
+            $validationMsgStr ="\n\t ".'$messages = [';
+            $validationStr ="\n\t".'$validator = Validator::make($request->all(), ['."\n";
+            $validateRule = false;
+            if(isset($mod->module_info->fields) && $mod->module_info->fields!=''){
+                $validationStrCon ='';
+                 $validationStrConEdit ='';
+                $fieldsArr = json_decode($mod->module_info->fields);
+                foreach($fieldsArr as $fieldKey => $fieldValue) {
+                     //$actualJsonFormat[$fieldKey]['validation'] = $fieldValue->name;
+
+                    
+                    if(isset($fieldValue->validation)){
+                        $validationValueStr = '';
+                        $validationMsgsStr = '';
+                        foreach($fieldValue->validation as $validationKey => $validationValue) {
+                           $validationValueStr.=$validationValue->rule.'|'; 
+                           $validationMsgsStr.="'".$fieldValue->name.".".$validationValue->rule."' => trans('".$module_config->slug_mob.".admin_".$module_config->slug_mob."_module_error_".$fieldValue->name."_".$validationValue->rule."'),"."\n";
+                        }
+                    
+                        $validationStr .= "\t\t"."'".$fieldValue->name."' => '".substr($validationValueStr,0,-1)."',";  
+                        $validationMsgStr .= $validationMsgsStr;
+                        $validateRule = true;
+                    } 
+                } 
+                $validationMsgStr .='];'."\n";
+                $validationStr = substr($validationStr,0,-1)."\n";
+                $validationStr .="\t".'],$messages);'."\n";
+                $validationStrCon .= 'if ($validator->fails()) {'."\n";
+                $validationStrCon .= 'return redirect(\'admin/'.str_plural($module_config->slug_mob).'/create\')->withErrors($validator)->withInput();'."\n";
+                
+                $validationStrCon .='}'."\n";
+                $validationStrConEdit .= 'if ($validator->fails()) {'."\n";
+                $validationStrConEdit .= 'return redirect(\'admin/'.str_plural($module_config->slug_mob).'/\'.$id.\'/edit\')->withErrors($validator)->withInput();'."\n";
+                $validationStrConEdit .='}'."\n";  
+            }
+
+            if($validateRule==true){
+                $validatorValues = $validationMsgStr."\n".$validationStr.$validationStrCon;
+                $validatorValuesEdit = $validationMsgStr."\n".$validationStr.$validationStrConEdit;
+            }else{
+                $validatorValues = '';
+                $validatorValuesEdit = '';
+            }    
+            $templateData = str_replace('$VAILDATIONS$', $validatorValues, $templateData);
+            $templateData = str_replace('$EDIT_VAILDATIONS$', $validatorValuesEdit, $templateData);
+       }else{
+         $templateData = get_template('scaffold.controller.controller_empty', 'laravel-generator');
+       }
         FileUtil::createFile($this->path, $this->fileName, $templateData);
 
         $this->commandData->commandComment("\nController created: ");
